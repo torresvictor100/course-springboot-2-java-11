324516eb182e33b9c82b16e762ee84a05e002964
torresvictor100 <torresvictor100@gmail.com>
2022-01-05 19:45:36 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,14 +1,21 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -20,6 +27,11 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String passaword;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	
 	
 	public User() {
@@ -33,14 +45,17 @@ public class User implements Serializable{
 		this.phone = phone;
 		this.passaword = passaword;
 	}
-
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	public Long getId() {
 		return id;
 	}
 
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -102,6 +117,9 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return id == other.id;
 	}
+
+
+	
 	
 	
 	
